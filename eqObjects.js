a2b3e46c4dc322b5909f12f095ab7512c11a7e4b
Wagsnerlund <chelseawagner17@gmail.com>
2021-03-04 05:33:22 +0000

    Use to determine if 2 objects are the same

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,27 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+const eqObjects = function (object1, object2) {
+  for (const property in object1) {
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+      return false;
+    } else if (!object2[property]) {
+      return false;
+    } else if (object1[property] !== object2[property]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertEqual((eqObjects(ab, ba)), true);
+assertEqual((eqObjects(ab, abc)), false);
